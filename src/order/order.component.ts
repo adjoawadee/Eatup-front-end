3b86e12d660ab605dea5f7dd40665a038be26ccd
adjoawadee <ernestina.wad@gmail.com>
2016-06-26 14:33:34 +0200

    LogIn response routed to order page

--- src/order/order.component.ts
+++ src/order/order.component.ts
@@ -15,10 +15,33 @@ import {SignInComponent} from "../sign-in/sign-in.component.ts";
 
 export class OrderComponent implements OnInit{
 
-    // private newUser = new User("","","",new Address("","","",""));
+    private newUser = new User("","","",new Address("","","",""));
+    private _localUser:User;
     constructor(private _registerService:RegisterService, private _router:Router, private _signInComponent:SignInComponent){}
 
     ngOnInit():any {
+
+
+        console.log(localStorage.getItem("user"));
+        this._localUser = JSON.parse(localStorage.getItem("user"));
+
+        if(this._localUser == null)
+        {
+            this.newUser = new User("null","null","null",new Address("null","null","null","null"));
+            alert("redirect to Login page");
+            this._router.navigate(['/sign-in']);
+        }
+        else
+        {
+            this.newUser = JSON.parse(localStorage.getItem("user"));
+            // this.newUser = new User(this._localUser.username, this._localUser.email, this._localUser.password, new Address(
+            //                         this._localUser.address.street, this._localUser.address.housenumber,
+            //                         this._localUser.address.addition, this._localUser.address.city
+            // ));
+            console.log(this.newUser);
+        }
+
+
         // console.log(this._signInComponent.user.address.city);
        // if(this._registerService.isLoggedIn==false)
        //      this._router.navigate(['/sign-in']);
